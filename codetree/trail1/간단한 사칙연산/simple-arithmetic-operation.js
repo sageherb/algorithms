550deccaebc8c78445114992098ee48d5e416299
Sage <106433812+sageherb@users.noreply.github.com>
2026-06-11 13:23:16 +0900

    [Passed] 간단한 사칙연산 (67ms, 10MB)

---
+++ codetree/trail1/간단한 사칙연산/simple-arithmetic-operation.js
@@ -0,0 +1,9 @@
+const fs = require("fs");
+let [a, b] = fs.readFileSync(0).toString().split(" ");
+a = Number(a);
+b = Number(b);
+
+console.log(a + b);
+console.log(a - b);
+console.log(parseInt(a / b));
+console.log(a % b);
