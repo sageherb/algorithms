fcf810c232d310717851f49a7bc31b0f6939d1f8
Sage <106433812+sageherb@users.noreply.github.com>
2026-06-11 16:52:48 +0900

    [Passed] 출석 부르기 (65ms, 10MB)

---
+++ codetree/trail1/출석 부르기/calling-attendance.js
@@ -0,0 +1,13 @@
+const fs = require("fs");
+let n = fs.readFileSync(0).toString().trim();
+n = Number(n);
+
+if (n === 1) {
+    console.log("John");
+} else if (n === 2) {
+    console.log("Tom");
+} else if (n === 3) {
+    console.log("Paul");
+} else {
+    console.log("Vacancy");
+}
