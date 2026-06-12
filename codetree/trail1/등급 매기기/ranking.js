ef5003b2fe4bd5cbd337d2b4142f23721767a291
Sage <106433812+sageherb@users.noreply.github.com>
2026-06-12 10:09:15 +0900

    [Passed] 등급 매기기 (62ms, 10MB)

---
+++ codetree/trail1/등급 매기기/ranking.js
@@ -0,0 +1,15 @@
+const fs = require("fs");
+let score = fs.readFileSync(0).toString().trim();
+score = Number(score);
+
+if (score >= 90) {
+    console.log("A");
+} else if (score >= 80) {
+    console.log("B");
+} else if (score >= 70) {
+    console.log("C");
+} else if (score >= 60) {
+    console.log("D");
+} else {
+    console.log("F");
+}
