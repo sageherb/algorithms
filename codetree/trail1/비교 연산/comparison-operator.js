119c963b704d1b0e0b27fa79dc0f1358a906bb13
Sage <106433812+sageherb@users.noreply.github.com>
2026-06-24 09:40:09 +0900

    [Passed] 비교 연산 (64ms, 10MB)

---
+++ codetree/trail1/비교 연산/comparison-operator.js
@@ -0,0 +1,11 @@
+const fs = require("fs");
+let [a, b] = fs.readFileSync(0).toString().split(" ");
+a = Number(a);
+b = Number(b);
+
+console.log(a >= b ? 1 : 0);
+console.log(a > b ? 1 : 0);
+console.log(b >= a ? 1 : 0);
+console.log(b > a ? 1 : 0);
+console.log(a === b ? 1 : 0);
+console.log(a !== b ? 1 : 0);
