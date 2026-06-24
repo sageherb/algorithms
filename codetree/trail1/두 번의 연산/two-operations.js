439b4fe7090c284e73e0bc46699a695f61dccf3b
Sage <106433812+sageherb@users.noreply.github.com>
2026-06-24 09:31:41 +0900

    [Passed] 두 번의 연산 (67ms, 10MB)

---
+++ codetree/trail1/두 번의 연산/two-operations.js
@@ -0,0 +1,13 @@
+const fs = require("fs");
+let a = fs.readFileSync(0).toString().trim();
+a = Number(a);
+
+if (a % 2 !== 0) {
+    a += 3;
+}
+
+if (a % 3 === 0) {
+    a /= 3;
+}
+
+console.log(a);
