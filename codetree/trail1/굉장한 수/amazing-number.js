8400f8e9ea7d23cbce87699ed4194781e3ad42e6
Sage <106433812+sageherb@users.noreply.github.com>
2026-07-01 09:27:56 +0900

    [Passed] 굉장한 수 (64ms, 10MB)

---
+++ codetree/trail1/굉장한 수/amazing-number.js
@@ -0,0 +1,9 @@
+const fs = require("fs");
+let n = fs.readFileSync(0).toString().trim();
+n = Number(n);
+
+if ((n % 2 === 1 && n % 3 === 0) || (n % 2 === 0 && n % 5 === 0)) {
+    console.log("true");
+} else {
+    console.log("false");
+}
