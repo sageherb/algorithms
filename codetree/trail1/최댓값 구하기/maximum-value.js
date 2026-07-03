86d774532cb3ee5832a84cec799e0d8b42440905
Sage <106433812+sageherb@users.noreply.github.com>
2026-07-03 11:17:11 +0900

    [Passed] 최댓값 구하기 (64ms, 10MB)

---
+++ codetree/trail1/최댓값 구하기/maximum-value.js
@@ -0,0 +1,13 @@
+const fs = require("fs");
+let [a, b, c] = fs.readFileSync(0).toString().split(" ");
+a = Number(a);
+b = Number(b);
+c = Number(c);
+
+if (a >= b && a >= c) {
+    console.log(a);
+} else if (b >= a && b >= c) {
+    console.log(b);
+} else {
+    console.log(c);
+}
